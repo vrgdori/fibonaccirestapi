690dbe5caa0161568e78a64fb39d02ed084258aa
hallgato <vargadori12@gmail.com>
2023-09-25 12:56:10 +0200

    done

--- src/main/kotlin/hu/obuda/devops/fibonaccirestapi/service/FibonacciService.kt
+++ src/main/kotlin/hu/obuda/devops/fibonaccirestapi/service/FibonacciService.kt
@@ -5,7 +5,11 @@ import org.springframework.stereotype.Service
 @Service
 class FibonacciService {
     fun fibonacci(n: Int): Int {
-        return if (n == 1) 0
-        else 0 // TODO instead of this logic implement fibonacci
+        return when (n) {
+            1 -> 1
+            2 -> 1
+            else -> fibonacci(n-1)+fibonacci(n-2)
+        }
+        // TODO instead of this logic implement fibonacci
     }
 }
--- src/test/kotlin/hu/obuda/devops/fibonaccirestapi/service/FibonacciServiceTest.kt
+++ src/test/kotlin/hu/obuda/devops/fibonaccirestapi/service/FibonacciServiceTest.kt
@@ -15,8 +15,53 @@ class FibonacciServiceTest {
         // when
         val result: Int = underTest.fibonacci(1)
         // then
-        Assertions.assertEquals(0, result)
+        Assertions.assertEquals(1, result)
     }
 
     // TODO - Test with greater numbers and test edge cases
+    @Test
+    fun shouldReturn0WhenCall2() {
+        // given
+
+        // when
+        val result: Int = underTest.fibonacci(2)
+        // then
+        Assertions.assertEquals(1, result)
+    }
+    @Test
+    fun shouldReturn0WhenCall3() {
+        // given
+
+        // when
+        val result: Int = underTest.fibonacci(3)
+        // then
+        Assertions.assertEquals(2, result)
+    }
+    @Test
+    fun shouldReturn0WhenCall4() {
+        // given
+
+        // when
+        val result: Int = underTest.fibonacci(4)
+        // then
+        Assertions.assertEquals(3, result)
+    }
+    @Test
+    fun shouldReturn0WhenCall5() {
+        // given
+
+        // when
+        val result: Int = underTest.fibonacci(5)
+        // then
+        Assertions.assertEquals(5, result)
+    }
+    @Test
+    fun shouldReturn0WhenCall6() {
+        // given
+
+        // when
+        val result: Int = underTest.fibonacci(6)
+        // then
+        Assertions.assertEquals(8, result)
+    }
 }
